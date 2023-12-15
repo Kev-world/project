3f3ad81427a88e2dc0ca9eaa1d8f82d4284654c9
Kev-world <hengkevin03@gmail.com>
2023-12-15 11:53:03 +0700

    update models

---
+++ dtos/projectDto.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+class CreateProjectDto(BaseModel):
+    title: str = Field(min_length=5)
+    description: str = Field(min_length=5, max_length=100)
+    date_created: datetime = None
--- models/model.py
+++ models/model.py
@@ -1,9 +1,11 @@
 from databases.sqlDB import Base
 from sqlalchemy import ARRAY, Column, ForeignKey, Integer, String, Boolean, Date
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 class Projects(Base):
     __tablename__ = 'projects'
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String(36), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     title = Column(String(50))
     description = Column(String(100))
     date_created = Column(Date)
@@ -11,10 +13,10 @@ class Projects(Base):
 
 class Tasks(Base):
     __tablename__ = 'tasks'
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String(36), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     content = Column(String(100))
     date_created = Column(Date)
     priority = Column(Integer)
     is_completed = Column(Boolean, default=False)
     assigned_to = Column(String(100), default="")
-    project_id = Column(Integer, ForeignKey('projects.id'))
+    project_id = Column(String(36), ForeignKey('projects.id'))
